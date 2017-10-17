b0b8c2882da26ce785ca313c7f2a4071b4c3f678
nyeo <nyeo@L03340MBE372.campus.aston.ac.uk>
2017-10-17 13:37:10 +0100

    Create project, add LetterBag class

---
+++ scrabble/src/scrabble/LetterBag.java
@@ -0,0 +1,11 @@
+package scrabble;
+
+/**
+ * LetterBag class provides the next random letter for each player. Can be used multiple times for each player in a turn.
+ * @author Ollie Nye
+ * @version 1.0
+ */
+
+public class LetterBag {
+
+}
