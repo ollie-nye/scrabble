0cb956e1e174d4e0f73cd1db3ed60fc7dafc6af8
nyeo <nyeo@L03340MBE372.campus.aston.ac.uk>
2017-10-17 14:54:47 +0100

    Continue adding methods to class

--- scrabble/src/scrabble/LetterBag.java
+++ scrabble/src/scrabble/LetterBag.java
@@ -2,6 +2,10 @@ package scrabble;
 
 import java.util.Stack;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Random;
+import java.util.ArrayList;
 
 /**
  * LetterBag class provides the next random letter for each player. Can be used multiple times for each player in a turn.
@@ -11,45 +15,77 @@ import java.util.HashMap;
 
 public class LetterBag {
 	
-	private static final int maxLetters = 150;
+	private static final int maxLetters = 100;
 	
 	private static final HashMap<String, Integer[]> letters = new HashMap<>();
+	
+	private ArrayList<String> letterList = new ArrayList<>();
+	
+	private Stack<String> letterStack = new Stack<>();
 	
 	/**
 	 * 
 	 */
 	public void LetterBag() {
-		
+		this.LetterBag(this.maxLetters);
 	}
 	
 	
 	public void LetterBag(int maxLetters) {
 		//put ( <letter>, (new Integer[]( <quantity in bag>, <score>))
-		this.letters.put("a", (new Integer[]{9, 1}));
-		this.letters.put("b", (new Integer[]{2, 3}));
-		this.letters.put("c", (new Integer[]{2, 3}));
-		this.letters.put("d", (new Integer[]{4, 2}));
-		this.letters.put("e", (new Integer[]{12, 1}));
-		this.letters.put("f", (new Integer[]{2, 4}));
-		this.letters.put("g", (new Integer[]{3, 2}));
-		this.letters.put("h", (new Integer[]{2, 4}));
-		this.letters.put("i", (new Integer[]{9, 1}));
-		this.letters.put("j", (new Integer[]{1, 8}));
-		this.letters.put("k", (new Integer[]{1, 5}));
-		this.letters.put("l", (new Integer[]{4, 1}));
-		this.letters.put("m", (new Integer[]{2, 3}));
-		this.letters.put("n", (new Integer[]{6, 1}));
-		this.letters.put("o", (new Integer[]{8, 1}));
-		this.letters.put("p", (new Integer[]{2, 3}));
-		this.letters.put("q", (new Integer[]{1, 10}));
-		this.letters.put("r", (new Integer[]{6, 1}));
-		this.letters.put("s", (new Integer[]{4, 1}));
-		this.letters.put("t", (new Integer[]{6, 1}));
-		this.letters.put("u", (new Integer[]{4, 1}));
-		this.letters.put("v", (new Integer[]{2, 4}));
-		this.letters.put("w", (new Integer[]{2, 4}));
-		this.letters.put("x", (new Integer[]{1, 8}));
-		this.letters.put("y", (new Integer[]{2, 4}));
-		this.letters.put("z", (new Integer[]{1, 10}));
+		letters.put("a", (new Integer[]{9, 1}));
+		letters.put("b", (new Integer[]{2, 3}));
+		letters.put("c", (new Integer[]{2, 3}));
+		letters.put("d", (new Integer[]{4, 2}));
+		letters.put("e", (new Integer[]{12, 1}));
+		letters.put("f", (new Integer[]{2, 4}));
+		letters.put("g", (new Integer[]{3, 2}));
+		letters.put("h", (new Integer[]{2, 4}));
+		letters.put("i", (new Integer[]{9, 1}));
+		letters.put("j", (new Integer[]{1, 8}));
+		letters.put("k", (new Integer[]{1, 5}));
+		letters.put("l", (new Integer[]{4, 1}));
+		letters.put("m", (new Integer[]{2, 3}));
+		letters.put("n", (new Integer[]{6, 1}));
+		letters.put("o", (new Integer[]{8, 1}));
+		letters.put("p", (new Integer[]{2, 3}));
+		letters.put("q", (new Integer[]{1, 10}));
+		letters.put("r", (new Integer[]{6, 1}));
+		letters.put("s", (new Integer[]{4, 1}));
+		letters.put("t", (new Integer[]{6, 1}));
+		letters.put("u", (new Integer[]{4, 1}));
+		letters.put("v", (new Integer[]{2, 4}));
+		letters.put("w", (new Integer[]{2, 4}));
+		letters.put("x", (new Integer[]{1, 8}));
+		letters.put("y", (new Integer[]{2, 4}));
+		letters.put("z", (new Integer[]{1, 10}));
+		
+		for (Map.Entry<String, Integer[]> entry : this.letters.entrySet()) {
+			String letter = entry.getKey();
+			Integer quantity = entry.getValue()[0];
+			Integer value = entry.getValue()[1];
+			
+			letterList.add(letter);
+		}
+		
+		Iterator<String> letter = letterList.iterator();
+		Random random = new Random();
+		while (letter.hasNext()) {
+			Integer nextLetter = random.nextInt(letterList.size() - 1);
+			String letterToPush = letterList.get(nextLetter);
+			letterList.remove(nextLetter);
+			letterStack.push(letterToPush);
+		}
+	}
+	
+	public String getNextLetter() {
+		return letterStack.pop();
+	}
+	
+	public static void main(String args[]) {
+		LetterBag bag = new LetterBag();
+		for (int i = 0; i < 100; i++) {
+			bag.getNextLetter();
+		}
 	}
 }
