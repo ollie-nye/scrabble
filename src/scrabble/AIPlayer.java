8bb2737cd0d2f632011e9f54c7a63773f2b71f4a
Thomas-Geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2017-11-03 14:53:52 +0000

    Added INCOMPLETE Player abstract class and sub classes.
    Added superclass Player
    Added subclass HumanPlayer
    Added subclass AIPlayer
    
    HumanPlayer & AIPlayer extend Player.

---
+++ src/scrabble/AIPlayer.java
@@ -0,0 +1,25 @@
+package scrabble;
+
+/**
+ * Contains methods specific to AIPlayer player type. Extends 'Player ' abstract class.
+ * @author Thomas Geraghty
+ * @version 1.0
+ */
+public class AIPlayer extends Player {
+
+    /**
+     * Constructor for AIPlayer, sets player username based on passed parameter 'name'.
+     * Adds 7 letters from LetterBag to player's collection of letters.
+     *
+     * @param   name    Player username string
+     * @see         scrabble.Player
+     */
+	public AIPlayer(String name) {
+		super();
+		setPlayerName(name);
+
+		for(int i = 0;  i <7; i++) {
+			addLetter();
+		}
+	}
+}
--- src/scrabble/Player.java
+++ src/scrabble/Player.java
@@ -1,29 +1,79 @@
 package scrabble;
-
-import java.util.ArrayList;
+import java.util.Random;
 
 /**
- * Contains all methods related to the player component
- * @author Ollie Nye
+ * Superclass containing shared methods related to player component of game.
+ * @author Thomas Geraghty
  * @version 1.1
  */
+abstract class  Player {
 
-public class Player {
-	
-	private static final int PLAY_LENGTH = 7;
+	private String[] letterList = new String[7];
+    private Random rnd = new Random();
+    private String playerName;
+    private int score = 0;
 
-	private static ArrayList<String> letters = new ArrayList<>();
-	
-	private String name = "";
-	
-	public void Player(String name) {
-		this.name = name;
-		refillLetters();
-	}
-	
-	private void refillLetters() {
-		while (letters.size() < PLAY_LENGTH) {
-			LetterBag.getInstance().pick();
-		}
-	}
+
+    /**
+     * Sets player username to appear in game.
+     * @see     #getPlayerName()
+     */
+    public void setPlayerName(String name) {
+        playerName = name;
+    }
+
+    /**
+     * Returns player username upon call
+     * @return  name    Player username String
+     * @see     #setPlayerName(String)
+     */
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    /**
+     * Set player's score to passed parameter amount..
+     * @param   score   Score to set.
+     * @see         #getScore()
+     */
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    /**
+     * Returns player's score on call.
+     * @return  score   Score represented as integer.
+     * @see         #setScore(int)
+     */
+    public int getScore() {
+        return score;
+    }
+
+    /**
+     *  Adds letter to players current collection of letters (letterList array).
+     *  Adds letter in first position free in players letter collection.
+     *  Gets chosen letter from LetterBag 'pick' method..
+     *
+     *  @see    LetterBag#pick()
+     */
+    public void addLetter() {
+        for(int i = 0; i < letterList.length; i++) {
+            if(letterList[i]  == null) {
+                letterList[i] = LetterBag.getInstance().pick();
+                break;
+            }
+        }
+    }
+
+	public void removeLetter() {
+    }
+
+
+    private void getLetter() {}
+
+
+    private void playLetter(String letter, int x, int y) {
+       // BoardArray[x][y] = letter;     - NOT YET IMPLEMENTED
+    }
+    private void selectedLetter(String letter, int x, int y) {}
 }
