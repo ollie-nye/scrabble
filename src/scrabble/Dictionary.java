5f5adf60e23e677bac4c1472c81c06df3c43f6c9
Asid <32871819+IdRatherBeaSnail@users.noreply.github.com>
2017-11-03 15:18:24 +0000

    Updated Dictionary.java
    Updated getWord() method 
    
    Updated comments

--- src/scrabble/Dictionary.java
+++ src/scrabble/Dictionary.java
@@ -9,29 +9,30 @@ public class Dictionary {
      private ArrayList<String> ListofWords;
      
      /*
-      * word file was taken from https://github.com/dwyl/english-words/blob/master/words_alpha.txt
-      * constructor fills arraylist with words from words.dat file.
+      * Word file was taken from https://github.com/dwyl/english-words/blob/master/words_alpha.txt
+      * and modified by Lorenzo to fill the project requirements.  
+      * constructor fills arraylist with words from the text file provided.
+      * @author  Asid Khan
+      * @version  1.0.1
      */
 public Dictionary() throws FileNotFoundException{
     	 
-    try (Scanner wordScanner = new Scanner(new File(getClass().getResource("words.dat").getFile()))){
+    try (Scanner wordScanner = new Scanner(new File(getClass().getResource("ScrabbleDicionary.txt").getFile()))){
          ListofWords = new ArrayList<>();
-    	  while(wordScanner.hasNext()){
-    			 ListofWords.add(wordScanner.next());
-    		 }	 
-    	 }
+    	 while(wordScanner.hasNext()){
+    	   ListofWords.add(wordScanner.next());
+    	  }	 
+    	}
      
-     }
+}
 // Value of many words are within the word list is returned
 public String getWord(int word){
-	return ListofWords.get(word);		
+	return ListofWords.get(word).toString();		
 }
 // The total size of arraylist is returned
 public int getSize(){
 	return ListofWords.size();	
 }
    }
-     
-     
 
 
