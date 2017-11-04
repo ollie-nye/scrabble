634e7ec73adc2d2dc631f132a8cb573149595c0a
Ollie <nyeo@aston.ac.uk>
2017-11-04 01:59:38 +0000

    Added data package for communications classes

---
+++ src/data/Coordinate.java
@@ -0,0 +1,15 @@
+package data;
+
+public class Coordinate {
+
+	private int x;
+	private int y;
+	
+	public Coordinate(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX() {return this.x;}
+	public int getY() {return this.y;}
+}
