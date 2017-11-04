aed3cbdc226f51e4a4ef4d0a452b8ff1a170d794
Ollie <nyeo@aston.ac.uk>
2017-11-04 00:30:20 +0000

    Add Tile and BoardScorer as data comms classes, amend Players to use new Tile classes

--- src/scrabble/Board.java
+++ src/scrabble/Board.java
@@ -1,5 +1,7 @@
 package scrabble;
 
+import player.Player;
+
 /**
  * Main class that holds the board and organises the game
  * @author Ollie Nye
@@ -7,5 +9,73 @@ package scrabble;
  */
 
 public class Board {
-
+	private Player player;
+	//private LetterScore letterScore; //TODO implement LetterScore
+	
+	// new BoardScorer(false, 2)
+	// new BoardScorer(False, 3)
+	// new BoardScorer(true, 2)
+	// new BoardScorer(true, 3)
+	
+	private char[][] types = new char[][]{
+		{'w', 'n', 'n', 'l', 'n', 'n', 'n', 'w', 'n', 'n', 'n', 'l', 'n', 'n', 'w'},
+		{'n', 'w', 'n', 'n', 'n', 'l', 'n', 'n', 'n', 'l', 'n', 'n', 'n', 'w', 'n'},
+		{'n', 'n', 'w', 'n', 'n', 'n', 'l', 'n', 'l', 'n', 'n', 'n', 'w', 'n', 'n'},
+		{'l', 'n', 'n', 'w', 'n', 'n', 'n', 'l', 'n', 'n', 'n', 'w', 'n', 'n', 'l'},
+		{'n', 'n', 'n', 'n', 'w', 'n', 'n', 'n', 'n', 'n', 'w', 'n', 'n', 'n', 'n'},
+		{'n', 'l', 'n', 'n', 'n', 'l', 'n', 'n', 'n', 'l', 'n', 'n', 'n', 'l', 'n'},
+		{'n', 'n', 'l', 'n', 'n', 'n', 'l', 'n', 'l', 'n', 'n', 'n', 'l', 'n', 'n'},
+		{'w', 'n', 'n', 'l', 'n', 'n', 'n', 'w', 'n', 'n', 'n', 'l', 'n', 'n', 'w'},
+		{'n', 'n', 'l', 'n', 'n', 'n', 'l', 'n', 'l', 'n', 'n', 'n', 'l', 'n', 'n'},
+		{'n', 'l', 'n', 'n', 'n', 'l', 'n', 'n', 'n', 'l', 'n', 'n', 'n', 'l', 'n'},
+		{'n', 'n', 'n', 'n', 'w', 'n', 'n', 'n', 'n', 'n', 'w', 'n', 'n', 'n', 'n'},
+		{'l', 'n', 'n', 'w', 'n', 'n', 'n', 'l', 'n', 'n', 'n', 'w', 'n', 'n', 'l'},
+		{'n', 'n', 'w', 'n', 'n', 'n', 'l', 'n', 'l', 'n', 'n', 'n', 'w', 'n', 'n'},
+		{'n', 'w', 'n', 'n', 'n', 'l', 'n', 'n', 'n', 'l', 'n', 'n', 'n', 'w', 'n'},
+		{'w', 'n', 'n', 'l', 'n', 'n', 'n', 'w', 'n', 'n', 'n', 'l', 'n', 'n', 'w'}
+	};
+	
+	private int[][] scores = new int[][]{
+		{'3', 'n', 'n', '2', 'n', 'n', 'n', '3', 'n', 'n', 'n', '2', 'n', 'n', '3'},
+		{'n', '2', 'n', 'n', 'n', '3', 'n', 'n', 'n', '3', 'n', 'n', 'n', '2', 'n'},
+		{'n', 'n', '2', 'n', 'n', 'n', '2', 'n', '2', 'n', 'n', 'n', '2', 'n', 'n'},
+		{'2', 'n', 'n', '2', 'n', 'n', 'n', '2', 'n', 'n', 'n', '2', 'n', 'n', '2'},
+		{'n', 'n', 'n', 'n', '2', 'n', 'n', 'n', 'n', 'n', '2', 'n', 'n', 'n', 'n'},
+		{'n', '3', 'n', 'n', 'n', '3', 'n', 'n', 'n', '3', 'n', 'n', 'n', '3', 'n'},
+		{'n', 'n', '2', 'n', 'n', 'n', '2', 'n', '2', 'n', 'n', 'n', '2', 'n', 'n'},
+		{'3', 'n', 'n', '2', 'n', 'n', 'n', '2', 'n', 'n', 'n', '2', 'n', 'n', '3'},
+		{'n', 'n', '2', 'n', 'n', 'n', '2', 'n', '2', 'n', 'n', 'n', '2', 'n', 'n'},
+		{'n', '3', 'n', 'n', 'n', '3', 'n', 'n', 'n', '3', 'n', 'n', 'n', '3', 'n'},
+		{'n', 'n', 'n', 'n', '2', 'n', 'n', 'n', 'n', 'n', '2', 'n', 'n', 'n', 'n'},
+		{'2', 'n', 'n', '2', 'n', 'n', 'n', '2', 'n', 'n', 'n', '2', 'n', 'n', '2'},
+		{'n', 'n', '2', 'n', 'n', 'n', '2', 'n', '2', 'n', 'n', 'n', '2', 'n', 'n'},
+		{'n', '2', 'n', 'n', 'n', '3', 'n', 'n', 'n', '3', 'n', 'n', 'n', '2', 'n'},
+		{'3', 'n', 'n', '2', 'n', 'n', 'n', '3', 'n', 'n', 'n', '2', 'n', 'n', '3'}
+	};
+	
+	
+	private BoardScorer[][] scoreBoard = new BoardScorer[][]{};
+	
+	
+	public Board() {
+		
+		//Initialise board scoring
+		for (int i = 0; i < 15; i++) {
+			for (int j = 0; j < 15; j++) {
+				boolean isLetter;
+				boolean isWord;
+				int score;
+				switch (types[i][j]) {
+				case 'l':
+					scoreBoard[i][j] = new BoardScorer(true, scores[i][j]);
+				case 'w':
+					scoreBoard[i][j] = new BoardScorer(false, scores[i][j]);
+				default:
+					scoreBoard[i][j] = null;
+				}
+			}
+		}
+	}
+	
+	
 }
---
+++ src/scrabble/BoardScorer.java
@@ -0,0 +1,19 @@
+package scrabble;
+
+public class BoardScorer {
+	private boolean type; //true for letter score, false for word score
+	private int score;
+	
+	public BoardScorer(boolean type, int score) {
+		this.type = type;
+		this.score = score;
+	}
+	
+	public boolean isLetterScore() {
+		return this.type;
+	}
+	
+	public int getMultiplier() {
+		return this.score;
+	}
+}
