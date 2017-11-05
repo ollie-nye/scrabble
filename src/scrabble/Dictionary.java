4a58d66cb12b259df9293243a02107886012154c
ollie <ollie@192.168.137.209>
2017-11-05 00:04:03 +0000

    Add search method to dictionary

--- src/scrabble/Dictionary.java
+++ src/scrabble/Dictionary.java
@@ -5,34 +5,44 @@ import java.io.FileNotFoundException;
 import java.util.*;
 
 public class Dictionary {
+
+	private ArrayList<String> ListofWords;
+
+	/*
+	 * Word file was taken from
+	 * https://github.com/dwyl/english-words/blob/master/words_alpha.txt and
+	 * modified by Lorenzo to fill the project requirements. constructor fills
+	 * arraylist with words from the text file provided.
+	 * @author Asid Khan
+	 * @version 1.0.1
+	 */
+	public Dictionary() throws FileNotFoundException {
+
+		try (Scanner wordScanner = new Scanner(new File(getClass().getResource("ScrabbleDicionary.txt").getFile()))) {
+			ListofWords = new ArrayList<>();
+			while (wordScanner.hasNext()) {
+				ListofWords.add(wordScanner.next());
+			}
+		}
+
+	}
+
+	// Value of many words are within the word list is returned
+	public String getWord(int word) {
+		return ListofWords.get(word).toString();
+	}
+
+	// The total size of arraylist is returned
+	public int getSize() {
+		return ListofWords.size();
+	}
 	
-     private ArrayList<String> ListofWords;
-     
-     /*
-      * Word file was taken from https://github.com/dwyl/english-words/blob/master/words_alpha.txt
-      * and modified by Lorenzo to fill the project requirements.  
-      * constructor fills arraylist with words from the text file provided.
-      * @author  Asid Khan
-      * @version  1.0.1
-     */
-public Dictionary() throws FileNotFoundException{
-    	 
-    try (Scanner wordScanner = new Scanner(new File(getClass().getResource("ScrabbleDicionary.txt").getFile()))){
-         ListofWords = new ArrayList<>();
-    	 while(wordScanner.hasNext()){
-    	   ListofWords.add(wordScanner.next());
-    	  }	 
-    	}
-     
+	public int searchList(String searchString) {
+		//Searches list for a matching regex, adds and returns running total.
+		int runningTotal = 0;
+		for (String word : ListofWords) {
+			if (word.matches(searchString)) {runningTotal += 1;}
+		}
+		return runningTotal;
+	}
 }
-// Value of many words are within the word list is returned
-public String getWord(int word){
-	return ListofWords.get(word).toString();		
-}
-// The total size of arraylist is returned
-public int getSize(){
-	return ListofWords.size();	
-}
-   }
-
-
