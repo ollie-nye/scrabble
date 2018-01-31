339cbf3c4da9f1605bad3334aa823dac89218561
Asid <32871819+IdRatherBeaSnail@users.noreply.github.com>
2018-01-31 19:06:03 +0000

    Libgdx folders ( core , desktop etc..)
    standard pre-designed folders of libgdx add to the classes as we go

---
+++ libgdx/desktop/src/com/scrabble/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.scrabble.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.scrabble.game.scrabbleMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new scrabbleMain(), config);
+	}
+}
