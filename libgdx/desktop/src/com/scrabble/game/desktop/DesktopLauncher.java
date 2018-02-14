0621da86d2854d72a0d9e748080f81ccd927a5a3
Asid <32871819+IdRatherBeaSnail@users.noreply.github.com>
2018-02-14 12:26:13 +0000

    Config for launcher
    set window icon and window size.

--- libgdx/desktop/src/com/scrabble/game/desktop/DesktopLauncher.java
+++ libgdx/desktop/src/com/scrabble/game/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.scrabble.game.desktop;
 
+import com.badlogic.gdx.Files.FileType;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.scrabble.game.scrabbleMain;
@@ -7,6 +8,11 @@ import com.scrabble.game.scrabbleMain;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.resizable = false;
+		config.title = "Scrabble: Origins";
+		config.width = 1280;
+		config.height = 720;
+		config.addIcon("icon.png", FileType.Internal);
 		new LwjglApplication(new scrabbleMain(), config);
 	}
 }
