c4d6acbc7e05bc898e50cbabab49e8bf6bbfb563
Asid <Asidkhan95@gmail.com>
2018-02-27 15:26:22 +0000

    Implemented basics graphics for MainMenu

--- libgdx/core/src/screens/Screens/MainMenuScreen.java
+++ libgdx/core/src/screens/Screens/MainMenuScreen.java
@@ -11,24 +11,22 @@ import screens.ScrabbleLauncher;
 
 public class MainMenuScreen implements Screen {
 	
-	private static final int PLAY_BUTTON_WIDTH = 70;
-	private static final int PLAY_BUTTON_HEIGHT = 50;
+	private static final int PLAY_BUTTON_WIDTH = 254;
+	private static final int PLAY_BUTTON_HEIGHT = 65;
 	
-	private static final int HELP_BUTTON_WIDTH = 70;
-	private static final int HELP_BUTTON_HEIGHT = 40;
+	private static final int HELP_BUTTON_WIDTH = 244;
+	private static final int HELP_BUTTON_HEIGHT = 69;
 	
-	private static final int EXIT_BUTTON_WIDTH = 60;
-	private static final int EXIT_BUTTON_HEIGHT = 30;
+	private static final int EXIT_BUTTON_WIDTH = 320;
+	private static final int EXIT_BUTTON_HEIGHT = 80;
 	
-	private static final int PLAY_BUTTON_Y = 150;
-	private static final int EXIT_BUTTON_Y = 50;
-	private static final int HELP_BUTTON_Y = 100;
+	private static final int PLAY_BUTTON_Y = 330;
+	private static final int EXIT_BUTTON_Y = 240;
+	private static final int HELP_BUTTON_Y = 160;
 	
-	private static final int TITLE_WIDTH = 200;
-	private static final int TITLE_HEIGHT = 200;
-	private static final int TITLE_Y = 230;
-	
-	
+	private static final int PLAY_BUTTON_X = 515;
+	private static final int EXIT_BUTTON_X = 480;
+	private static final int HELP_BUTTON_X = 520;
 	
 	ScrabbleLauncher game;
 	
@@ -50,20 +48,16 @@ public class MainMenuScreen implements Screen {
 		
 		this.game= game;
 		
-		background = new Texture("backgroundmain.jpg");
-		logo = new Texture("scrabble.png");
-		
-		
+		background = new Texture("mainBackground.png");
 
-		
-		playButtonActive = new Texture("playPressed.png");
+		playButtonActive = new Texture("play.png");
 		playButtonInactive = new Texture("play.png");
 		
-		helpButtonActive = new Texture("helpPressed.png");
+		helpButtonActive = new Texture("help.png");
 		helpButtonInactive = new Texture("help.png");
 		
-		exitButtonActive = new Texture("exitPressed.png");
-		exitButtonInactive = new Texture("exit.png");
+		exitButtonActive = new Texture("settings.png");
+		exitButtonInactive = new Texture("settings.png");
 		
 		
 		
@@ -85,41 +79,41 @@ public class MainMenuScreen implements Screen {
 		
 		
 		game.batch.draw(background,0,0,ScrabbleLauncher.WIDTH,ScrabbleLauncher.HEIGHT);
-		game.batch.draw(logo,340,TITLE_Y,TITLE_WIDTH,TITLE_HEIGHT);
-		int x = 400;
+		
+		
 	
 		// creates the play button with user input to start the game and hover-over effects
 		
-		if (Gdx.input.getX() < x + PLAY_BUTTON_WIDTH  && Gdx.input.getX() > x && ScrabbleLauncher.HEIGHT - Gdx.input.getY() < PLAY_BUTTON_Y + PLAY_BUTTON_HEIGHT && ScrabbleLauncher.HEIGHT - Gdx.input.getY() > PLAY_BUTTON_Y ) {
-		game.batch.draw(playButtonActive, x , PLAY_BUTTON_Y, PLAY_BUTTON_WIDTH,PLAY_BUTTON_HEIGHT);
+		if (Gdx.input.getX() < PLAY_BUTTON_X + PLAY_BUTTON_WIDTH  && Gdx.input.getX() > PLAY_BUTTON_X && ScrabbleLauncher.HEIGHT - Gdx.input.getY() < PLAY_BUTTON_Y + PLAY_BUTTON_HEIGHT && ScrabbleLauncher.HEIGHT - Gdx.input.getY() > PLAY_BUTTON_Y ) {
+		game.batch.draw(playButtonActive, PLAY_BUTTON_X , PLAY_BUTTON_Y, PLAY_BUTTON_WIDTH,PLAY_BUTTON_HEIGHT);
 		if (Gdx.input.isTouched()) {
 			game.setScreen(new scrabbleMain(game));
 		
 		}
 		} else { 
-		game.batch.draw(playButtonInactive, x ,PLAY_BUTTON_Y, PLAY_BUTTON_WIDTH,PLAY_BUTTON_HEIGHT);		
+		game.batch.draw(playButtonInactive, PLAY_BUTTON_X ,PLAY_BUTTON_Y, PLAY_BUTTON_WIDTH,PLAY_BUTTON_HEIGHT);		
 		}
 		
 		// creates the help button with user input to open the help screen and hover-over effects
 		
-		if (Gdx.input.getX() < x + HELP_BUTTON_WIDTH  && Gdx.input.getX() > x && ScrabbleLauncher.HEIGHT - Gdx.input.getY() < HELP_BUTTON_Y + HELP_BUTTON_HEIGHT && ScrabbleLauncher.HEIGHT - Gdx.input.getY() > HELP_BUTTON_Y ) {
-		game.batch.draw(helpButtonActive, x , HELP_BUTTON_Y, HELP_BUTTON_WIDTH,HELP_BUTTON_HEIGHT);
+		if (Gdx.input.getX() < HELP_BUTTON_X + HELP_BUTTON_WIDTH  && Gdx.input.getX() > HELP_BUTTON_X && ScrabbleLauncher.HEIGHT - Gdx.input.getY() < HELP_BUTTON_Y + HELP_BUTTON_HEIGHT && ScrabbleLauncher.HEIGHT - Gdx.input.getY() > HELP_BUTTON_Y ) {
+		game.batch.draw(helpButtonActive, HELP_BUTTON_X , HELP_BUTTON_Y, HELP_BUTTON_WIDTH,HELP_BUTTON_HEIGHT);
 		if (Gdx.input.isTouched()) {
 			game.setScreen(new HelpScreen(game));
 		}
 		} else { 
-		game.batch.draw(helpButtonInactive, x ,HELP_BUTTON_Y, HELP_BUTTON_WIDTH,HELP_BUTTON_HEIGHT);		
+		game.batch.draw(helpButtonInactive, HELP_BUTTON_X ,HELP_BUTTON_Y, HELP_BUTTON_WIDTH,HELP_BUTTON_HEIGHT);		
 		}
 		
 		// creates the exit button with user input to close the game and hover-over effects
 		
-		if (Gdx.input.getX() < x + EXIT_BUTTON_WIDTH  && Gdx.input.getX() > x && ScrabbleLauncher.HEIGHT - Gdx.input.getY() < EXIT_BUTTON_Y + EXIT_BUTTON_HEIGHT && ScrabbleLauncher.HEIGHT - Gdx.input.getY() > EXIT_BUTTON_Y ) {
-		game.batch.draw(exitButtonActive, x , EXIT_BUTTON_Y, EXIT_BUTTON_WIDTH,EXIT_BUTTON_HEIGHT);
+		if (Gdx.input.getX() < EXIT_BUTTON_X + EXIT_BUTTON_WIDTH  && Gdx.input.getX() > EXIT_BUTTON_X && ScrabbleLauncher.HEIGHT - Gdx.input.getY() < EXIT_BUTTON_Y + EXIT_BUTTON_HEIGHT && ScrabbleLauncher.HEIGHT - Gdx.input.getY() > EXIT_BUTTON_Y ) {
+		game.batch.draw(exitButtonActive, EXIT_BUTTON_X , EXIT_BUTTON_Y, EXIT_BUTTON_WIDTH,EXIT_BUTTON_HEIGHT);
 			if (Gdx.input.isTouched()) {
 				Gdx.app.exit();			
 			}
 		} else { 
-		game.batch.draw(exitButtonInactive, x ,EXIT_BUTTON_Y, EXIT_BUTTON_WIDTH,EXIT_BUTTON_HEIGHT);		
+		game.batch.draw(exitButtonInactive, EXIT_BUTTON_X ,EXIT_BUTTON_Y, EXIT_BUTTON_WIDTH,EXIT_BUTTON_HEIGHT);		
 		}
 				
 		
