f30cdb9387308c4ec821d2cb0e63c6a322bbe52b
Ben <Ben@bmiller>
2018-03-11 21:19:38 +0000

    current progress of adding number of players

--- libgdx/core/src/screens/Screens/MainMenu.java
+++ libgdx/core/src/screens/Screens/MainMenu.java
@@ -1,5 +1,8 @@
 package screens.Screens;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.audio.Sound;
@@ -11,9 +14,11 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Slider;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.scrabble.game.scrabbleMain;
 
@@ -34,6 +39,8 @@ public class MainMenu implements Screen {
 	private Sound hover;
 	private Texture background;
 	private BitmapFont font;
+	private HashMap<String, TextButton> playOptionsArray;
+	private Table playOptions;
 	
 	public MainMenu(ScrabbleLauncher game){
           
@@ -42,6 +49,7 @@ public class MainMenu implements Screen {
 		 background = game.getAssetManager().manager.get(assetManager.mainBackground);
 		 stage = new Stage(new ScreenViewport());
 		 font = new BitmapFont();
+		 playOptionsArray = new HashMap<String, TextButton>(); 
 
 	}
 
@@ -147,6 +155,51 @@ public class MainMenu implements Screen {
 				}		
 		});
 		stage.addActor(website);
+		
+		playOptions = new Table();			
+		playOptions.setPosition(640.0f, 120.0f);
+		
+		TextButton start = new TextButton("", playButtonStyle);
+		start.setSize(254.0f, 65.0f);
+		website.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {				
+				
+				}		
+		});		
+			
+		
+		
+		TextButton playersBox = new TextButton("0", playButtonStyle);
+		playersBox.setSize(127.0f, 127.0f);
+		website.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {				
+				
+				}		
+		});		
+			
+		
+		
+		TextButton aIBox = new TextButton("0", playButtonStyle);
+		aIBox.setSize(127.0f, 127.0f);
+		website.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {				
+				
+				}		
+		});		
+		
+		playOptionsArray.put("Start", start);	
+		playOptionsArray.put("PlayersBox", playersBox);	
+		playOptionsArray.put("AIBox", aIBox);	
+		playOptions.add(playOptionsArray.get("PlayersBox")).size(127.0f, 127.0f);
+		playOptions.add(playOptionsArray.get("AIBox"));
+		playOptions.row();
+		playOptions.add(playOptionsArray.get("Start"));
+		
+		stage.addActor(playOptions);
+		playOptions.setVisible(false);
 		
 	}
 
