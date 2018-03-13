e4320b55d5293354e20b271d6306d53065fc231e
Thomas-Geraghty <thomascgeraghty@outlook.com>
2018-03-13 16:03:19 +0000

    Added word multiplier implementation. Added some JavaDoc

--- libgdx/core/src/data/Move.java
+++ libgdx/core/src/data/Move.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 
 /**
  * @author Tom Geraghty
- * @version 1.0
+ * @version 1.1
  */
 public class Move {
     private final static Score SCORE_CALCULATOR = new Score();
@@ -15,35 +15,60 @@ public class Move {
     private Player player;
     private String playedWord;
     private int moveScore;
+    private int wordMultiplier = 1;
 
     public Move(Player player) {
         this.player = player;
     }
 
+    /**
+     * Add a Tile to Move (as in Tile has been played in this Move).
+     *
+     * @param   tile          Tile that's been played
+     * @param   coordinate    Coordinate of played Tile
+     */
     public void addTile(Tile tile, Coordinate coordinate) {
         playedTiles.put(tile, coordinate);
         moveScore += SCORE_CALCULATOR.calculateScore(tile, coordinate);
+        wordMultiplier *= SCORE_CALCULATOR.getWordMultiplier(coordinate);
     }
 
+    /**
+     * Removes Tile from Move (Tile has been returned to Player hand and has not been played)
+     *
+     * @param   tile          Tile to remove
+     * @param   coordinate    Coordinate of Tile to remove.
+     */
     public void removeTile(Tile tile, Coordinate coordinate) {
-        playedTiles.remove(coordinate);
+        playedTiles.remove(tile);
         moveScore -= SCORE_CALCULATOR.calculateScore(tile, coordinate);
     }
 
+    /**
+     * Returns all Tiles played in this Move.
+     *
+     * @return  tiles        Tiles played in this Move.
+     */
     public HashMap<Tile, Coordinate> getPlayedTiles() {
         return playedTiles;
     }
 
+    /**
+     * Returns current score of Move.
+     *
+     * @return  score       Current score of Move.
+     */
     public int getMoveScore() {
-        return moveScore;
+        return moveScore * wordMultiplier;
     }
 
-    private void setPlayedWord(String playedWord) {
+    /**
+     * Set the word played in this Move.
+     *
+     * @param   playedWord      The word played in this Move.
+     */
+    public void setPlayedWord(String playedWord) {
         this.playedWord = playedWord;
-    }
-
-    public void endMove(String playedWord) {
-        setPlayedWord(playedWord);
     }
 
     public String getPlayedWord() {
--- libgdx/core/src/scrabble/Score.java
+++ libgdx/core/src/scrabble/Score.java
@@ -5,33 +5,41 @@ import data.Tile;
 
 /**
  * @author Tom Geraghty
- * @version 1.1
- * */
+ * @version 1.2
+ */
 public class Score {
 
     /**
      * Types of multiplier tiles on the board
+     *
+     * n - standard place
+     * l - letter multiplier
+     * w - word multiplier
      */
     private char[][] types = new char[][]{
-            {'w','n','n','l','n','n','n','w','n','n','n','l','n','n','w'},
-            {'n','w','n','n','n','l','n','n','n','l','n','n','n','w','n'},
-            {'n','n','w','n','n','n','l','n','l','n','n','n','w','n','n'},
-            {'l','n','n','w','n','n','n','l','n','n','n','w','n','n','l'},
-            {'n','n','n','n','w','n','n','n','n','n','w','n','n','n','n'},
-            {'n','l','n','n','n','l','n','n','n','l','n','n','n','l','n'},
-            {'n','n','l','n','n','n','l','n','l','n','n','n','l','n','n'},
-            {'w','n','n','l','n','n','n','w','n','n','n','l','n','n','w'},
-            {'n','n','l','n','n','n','l','n','l','n','n','n','l','n','n'},
-            {'n','l','n','n','n','l','n','n','n','l','n','n','n','l','n'},
-            {'n','n','n','n','w','n','n','n','n','n','w','n','n','n','n'},
-            {'l','n','n','w','n','n','n','l','n','n','n','w','n','n','l'},
-            {'n','n','w','n','n','n','l','n','l','n','n','n','w','n','n'},
-            {'n','w','n','n','n','l','n','n','n','l','n','n','n','w','n'},
-            {'w','n','n','l','n','n','n','w','n','n','n','l','n','n','w'}
+            {'w', 'n', 'n', 'l', 'n', 'n', 'n', 'w', 'n', 'n', 'n', 'l', 'n', 'n', 'w'},
+            {'n', 'w', 'n', 'n', 'n', 'l', 'n', 'n', 'n', 'l', 'n', 'n', 'n', 'w', 'n'},
+            {'n', 'n', 'w', 'n', 'n', 'n', 'l', 'n', 'l', 'n', 'n', 'n', 'w', 'n', 'n'},
+            {'l', 'n', 'n', 'w', 'n', 'n', 'n', 'l', 'n', 'n', 'n', 'w', 'n', 'n', 'l'},
+            {'n', 'n', 'n', 'n', 'w', 'n', 'n', 'n', 'n', 'n', 'w', 'n', 'n', 'n', 'n'},
+            {'n', 'l', 'n', 'n', 'n', 'l', 'n', 'n', 'n', 'l', 'n', 'n', 'n', 'l', 'n'},
+            {'n', 'n', 'l', 'n', 'n', 'n', 'l', 'n', 'l', 'n', 'n', 'n', 'l', 'n', 'n'},
+            {'w', 'n', 'n', 'l', 'n', 'n', 'n', 'w', 'n', 'n', 'n', 'l', 'n', 'n', 'w'},
+            {'n', 'n', 'l', 'n', 'n', 'n', 'l', 'n', 'l', 'n', 'n', 'n', 'l', 'n', 'n'},
+            {'n', 'l', 'n', 'n', 'n', 'l', 'n', 'n', 'n', 'l', 'n', 'n', 'n', 'l', 'n'},
+            {'n', 'n', 'n', 'n', 'w', 'n', 'n', 'n', 'n', 'n', 'w', 'n', 'n', 'n', 'n'},
+            {'l', 'n', 'n', 'w', 'n', 'n', 'n', 'l', 'n', 'n', 'n', 'w', 'n', 'n', 'l'},
+            {'n', 'n', 'w', 'n', 'n', 'n', 'l', 'n', 'l', 'n', 'n', 'n', 'w', 'n', 'n'},
+            {'n', 'w', 'n', 'n', 'n', 'l', 'n', 'n', 'n', 'l', 'n', 'n', 'n', 'w', 'n'},
+            {'w', 'n', 'n', 'l', 'n', 'n', 'n', 'w', 'n', 'n', 'n', 'l', 'n', 'n', 'w'}
     };
 
     /**
      * Scores of multiplier tiles on the board
+     *
+     * 0 - no multiplication
+     * 2 - 2x multiplication
+     * 3 - 3x multiplication
      */
     private int[][] scores = new int[][]{
             {3, 0, 0, 2, 0, 0, 0, 3, 0, 0, 0, 2, 0, 0, 3},
@@ -52,21 +60,35 @@ public class Score {
     };
 
     /**
-     * Takes a letter and position, and returns its
+     * Takes a Tile and its Coordinate, and returns its
      * board value (i.e with double letter score).
-     * @param tile
-     * @param coordinate
-     * @return
+     *
+     * @param tile          Tile to score
+     * @param coordinate    Position of tile
+     * @return score        Board score of tile
      */
     public int calculateScore(Tile tile, Coordinate coordinate) {
-    	switch (types[coordinate.getX()][coordinate.getY()]) {
-    	case 'l': // letter multiplier
-    		return tile.getScore() * scores[coordinate.getX()][coordinate.getY()];
-    	case 'n': default:
-    		return tile.getScore();
-    	//TODO word multiplier - needs to be handled
-    	case 'w':
-			return tile.getScore();
-    	}
+        switch (types[coordinate.getX()][coordinate.getY()]) {
+            case 'l':
+                return tile.getScore() * scores[coordinate.getX()][coordinate.getY()];
+            case 'n': default:
+                return tile.getScore();
+        }
+    }
+
+    /**
+     * Returns the multiplier (if any) at the Coordinate.
+     * Returns 1 if no multiplier, returns up 2/3 if multiplier.
+     *
+     * @param coordinate       Coordinate to check for multiplier
+     * @return multiplier      Multiplier value
+     */
+    public int getWordMultiplier(Coordinate coordinate) {
+        switch (types[coordinate.getX()][coordinate.getY()]) {
+            case 'w':
+                return scores[coordinate.getX()][coordinate.getY()];
+            default:
+                return 1;
+        }
     }
 }
