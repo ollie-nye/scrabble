1535f9ab4f6adcfa04579c4b6cc6869204c1529f
Ben <Ben@bmiller>
2018-03-15 11:01:15 +0000

    results screen updates

--- libgdx/core/src/screens/screens/ResultsScreen.java
+++ libgdx/core/src/screens/screens/ResultsScreen.java
@@ -16,6 +16,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 import assetmanager.assetManager;
@@ -34,6 +35,7 @@ public class ResultsScreen implements Screen {
 	private Table resultsTable;
 	private Stage stage;
 	private Texture background;
+	private LabelStyle labelStyle, labelStyle1, labelStyle2; 
 
 	public ResultsScreen(ScrabbleLauncher game) {
 
@@ -49,74 +51,20 @@ public class ResultsScreen implements Screen {
 		skin.addRegions(buttonAtlas);
 		background = game.getAssetManager().manager.get(assetManager.resultsBackground);
 		font = game.getAssetManager().manager.get(assetManager.PlayTime);
+		styleSetups();
 	}
 
 	@Override
 	public void show() {
-		LabelStyle labelStyle = new LabelStyle();
-		labelStyle.font = font;
-		labelStyle.background = skin.getDrawable("green");
-
-		LabelStyle labelStyle1 = new LabelStyle();
-		labelStyle1.font = font;
-		labelStyle1.background = skin.getDrawable("lightblue");
-
-		LabelStyle labelStyle2 = new LabelStyle();
-		labelStyle2.font = font;
-		labelStyle2.background = skin.getDrawable("yellow");
-
+			
 		resultsTable = new Table();
-		resultsTable.setBackground(skin.getDrawable("purple"));
-
-		// +CREATING FULL PLAYER SCORECARD
-		Table player = new Table();
-		player.pad(25.0f);
-
-		// player names at top of players box;
-		// ++PLAYER NAME ROW
-		Label playerName = new Label("P1 and stuff", labelStyle);
-		resultsTable.add(playerName).colspan(2);
-		resultsTable.row();
-		// --PLAYER NAME ROW
-
-		// this is the list of letters that the player had left;
-		// ++LIST OF TILES LEFT FOR PLAYER
-		Table letters = new Table();
-		Label[] lettersLeft = new Label[7];
-		boolean style = true;
-		for (Label letter : lettersLeft) {
-			letter = new Label("a", labelStyle1);
-			letter.setText("a");
-			letters.add(letter);
-		}
-		resultsTable.add(letters).colspan(2);
-		resultsTable.row();
-		// --LIST OF TILES LEFT FOR PLAYER
-
-		// this shows current and score to be taken away
-		// ++SCORES TO CALCULATE TITLES
-		Label currentScoreTitle = new Label("Current Score", labelStyle);
-		Label scoreLossTitle = new Label("Penalties", labelStyle2);
-		resultsTable.add(currentScoreTitle);
-		resultsTable.add(scoreLossTitle);
-		resultsTable.row();
-		// --SCORES TO CALCULATE TITLES
 		
-		// this shows the actual score numbers
-		// ++SCORES TO CALCULATE
-		Label currentScore = new Label("67", labelStyle1);
-		Label scoreLoss = new Label("12", labelStyle2);
-		resultsTable.add(currentScore);
-		resultsTable.add(scoreLoss);
-		resultsTable.row();		
-		// --SCORES TO CALCULATE
-		
-		// ++FINAL SCORES
-		Label finalScore = new Label("Final Score: " + "54", labelStyle);
-		resultsTable.add(finalScore).colspan(2);
-		// --FINAL SCORES
-		
-		
+		resultsTable.add(playerScorecard("p1"));
+	    resultsTable.add(playerScorecard("Mike"));
+	    resultsTable.row();
+	    resultsTable.add(playerScorecard("p1"));
+	    resultsTable.add(playerScorecard("p1"));
+		resultsTable.pack();
 		
 		
 		resultsTable.setPosition((1280.0f - resultsTable.getWidth()) * 0.5f,
@@ -153,6 +101,84 @@ public class ResultsScreen implements Screen {
 
 	@Override
 	public void dispose() {
+	}
+	private void styleSetups(){
+		labelStyle = new LabelStyle();
+		labelStyle.font = font;
+		labelStyle.background = skin.getDrawable("green");
+
+		labelStyle1 = new LabelStyle();
+		labelStyle1.font = font;
+		labelStyle1.background = skin.getDrawable("lightblue");
+
+		labelStyle2 = new LabelStyle();
+		labelStyle2.font = font;
+		labelStyle2.background = skin.getDrawable("yellow");
+		
+	}
+	
+	private Table playerScorecard(String name){
+		
+		// +CREATING FULL PLAYER SCORECARD
+		Table player = new Table();
+		player.pad(15.0f);
+	
+		player.setBackground(skin.getDrawable("purple"));
+	
+		// player names at top of players box;
+		// ++PLAYER NAME ROW
+		Label playerName = new Label(name, labelStyle);
+		playerName.setAlignment(Align.center);
+		player.add(playerName).colspan(2).expandX().width(500.0f);
+		player.row();
+		
+		// --PLAYER NAME ROW
+
+		// this is the list of letters that the player had left;
+		// ++LIST OF TILES LEFT FOR PLAYER
+		Table letters = new Table();		
+		Label[] lettersLeft = new Label[7];		
+		for (Label letter : lettersLeft) {
+			letter = new Label("a", labelStyle1);
+			letter.setText("a");
+			letter.setFontScale(0.7f);			
+			letter.setAlignment(Align.center);
+			letters.add(letter).size(40.0f, 40.0f).expand();
+		}			
+		player.add(letters).colspan(2).expandX().fill();
+		letters.pack();		
+		player.row();
+		// --LIST OF TILES LEFT FOR PLAYER
+
+		// this shows current and score to be taken away
+		// ++SCORES TO CALCULATE TITLES
+		Label currentScoreTitle = new Label("Current Score", labelStyle);
+		Label scoreLossTitle = new Label("Penalties", labelStyle2);
+		currentScoreTitle.setAlignment(Align.center);
+		scoreLossTitle.setAlignment(Align.center);
+		player.add(currentScoreTitle).uniform().fill();
+		player.add(scoreLossTitle).uniform().fill();
+		player.row();
+		// --SCORES TO CALCULATE TITLES
+		
+		// this shows the actual score numbers
+		// ++SCORES TO CALCULATE
+		Label currentScore = new Label("67", labelStyle1);
+		Label scoreLoss = new Label("12", labelStyle);
+		currentScore.setAlignment(Align.center);
+		scoreLoss.setAlignment(Align.center);
+		player.add(currentScore).expand().fill();
+		player.add(scoreLoss).expand().fill();
+		player.row();		
+		// --SCORES TO CALCULATE
+		
+		// ++FINAL SCORES
+		Label finalScore = new Label("Final Score: " + "54", labelStyle);
+		finalScore.setAlignment(Align.center);
+		player.add(finalScore).colspan(2).expandX().fill();
+		// --FINAL SCORES
+		player.pack();
+		return player;
 	}
 
 }
