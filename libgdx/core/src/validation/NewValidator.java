a4736bd31cd72077c8380c3cdd293ba32e6e91ed
Ben <Ben@bmiller>
2018-04-29 20:09:03 +0100

    fixing second out of bounds bug

--- libgdx/core/src/validation/NewValidator.java
+++ libgdx/core/src/validation/NewValidator.java
@@ -15,7 +15,10 @@ import java.util.Map.Entry;
 
 public class NewValidator implements Serializable {
 
-	private enum Direction { HORIZONTAL, VERTICAL }
+	private enum Direction {
+		HORIZONTAL, VERTICAL
+	}
+
 	private Direction direction;
 	private int location = 0;
 	private Dawg dictionary = new Dawg();
@@ -38,14 +41,14 @@ public class NewValidator implements Serializable {
 		boolean allowedMove = true;
 		boolean emptySpace = (this.board.getTile(letter.getLocation()) == null);
 		int possibleWords = 0;
-		
 
 		if (emptySpace) {
-			
-			this.board.testPlace(letter); //Happens every turn
+
+			this.board.testPlace(letter); // Happens every turn
 			if (playedTiles == 0) {
-				//test row/column if not first move
-				if(Game.getMoveList().size() > 1) { //not first move of the game
+				// test row/column if not first move
+				if (Game.getMoveList().size() > 1) { // not first move of the
+														// game
 					int x = letter.getLocation().getX();
 					int y = letter.getLocation().getY();
 					boolean commonDirection = false;
@@ -78,17 +81,29 @@ public class NewValidator implements Serializable {
 				} else {
 					firstMove = letter.getLocation();
 					firstMoveContent = letter.getTile().getContent();
-				}		
+				}
 			} else if (playedTiles == 1) { // Second move
-				// Check that at least one of the coordinates is common between this and the last move, therefore determining direction
-				if (letter.getLocation().getX() == firstMove.getX()) { // X is common, therefore vertical word direction
+				// Check that at least one of the coordinates is common between
+				// this and the last move, therefore determining direction
+				if (letter.getLocation().getX() == firstMove.getX()) { // X is
+																		// common,
+																		// therefore
+																		// vertical
+																		// word
+																		// direction
 					possibleWords = testMove(letter);
 					if (possibleWords > 0) {
 						this.direction = Direction.VERTICAL;
 					} else {
 						allowedMove = false;
 					}
-				} else if (letter.getLocation().getY() == firstMove.getY()) { // Y is common, therefore horizontal word direction
+				} else if (letter.getLocation().getY() == firstMove.getY()) { // Y
+																				// is
+																				// common,
+																				// therefore
+																				// horizontal
+																				// word
+																				// direction
 					possibleWords = testMove(letter);
 					if (possibleWords > 0) {
 						this.direction = Direction.HORIZONTAL;
@@ -123,84 +138,80 @@ public class NewValidator implements Serializable {
 			playedTiles++;
 			possibleWords = 0;
 		} else {
-			//this.board.removeTile(letter.getLocation());
+			// this.board.removeTile(letter.getLocation());
 			Game.getCurrentMove().removeTile(letter.getTile());
 		}
 		this.result = new Result(allowedMove, possibleWords, testEndTurnMove(letter));
 		this.words = null;
 		return this.result;
 	}
-	
+
 	public boolean testConnectedWords(Move currentMove) {
 		HashMap<Tile, Coordinate> word = currentMove.getPlayedTiles();
 		boolean connected = false;
 		ArrayList<Coordinate> coordinates = new ArrayList<>();
 
-        if(Game.getMoveList().size() == 1) {
-            return true;
-        } else {
-        	boolean allowed = false;
-            for (Entry<Tile, Coordinate> letter : word.entrySet()) {
-            	Tile nextTile = board.getTile(letter.getValue().getNear('U'));
-            	if (!Game.getCurrentMove().getPlayedTiles().containsKey(nextTile)) {
-            		if (nextTile != null) {
-                        allowed = true;
-                        break;
-            		}
-            	}
-            	nextTile = board.getTile(letter.getValue().getNear('D'));
-            	if (!Game.getCurrentMove().getPlayedTiles().containsKey(nextTile)) {
-            		if (nextTile != null) {
-                        allowed = true;
-                        break;
-            		}
-            	}
-            	nextTile = board.getTile(letter.getValue().getNear('L'));
-            	System.out.println("Tile left is " + (nextTile==null?"empty":nextTile.toString()));
-            	if (!Game.getCurrentMove().getPlayedTiles().containsKey(nextTile)) {
-            		if (nextTile != null) {
-                        allowed = true;
-                        break;
-            		}
-            	}
-            	nextTile = board.getTile(letter.getValue().getNear('R'));
-            	if (!Game.getCurrentMove().getPlayedTiles().containsKey(nextTile)) {
-            		if (nextTile != null) {
-                        allowed = true;
-                        break;
-            		}
-            	}
-                continue;
-            }
-            if (allowed) {
-            	return true;
-            }
-        }
-        return false;
-
-		/*
-		if(Game.getMoveList().size() == 1) {
+		if (Game.getMoveList().size() == 1) {
 			return true;
 		} else {
-			for (Entry<Tile, Coordinate> entry : word.entrySet()) {
-				Coordinate coordinate = entry.getValue();
-				coordinates.add(coordinate);
-			}
-			for (Coordinate coordinate : coordinates) {
-				for (Coordinate neighbour : getSurroundingCoordinates(coordinate)) {
-					if (!coordinates.contains(neighbour)) {
-						connected = true;
+			boolean allowed = false;
+			for (Entry<Tile, Coordinate> letter : word.entrySet()) {
+				Tile nextTile = board.getTile(letter.getValue().getNear('U'));
+				if (!Game.getCurrentMove().getPlayedTiles().containsKey(nextTile)) {
+					if (nextTile != null) {
+						allowed = true;
 						break;
 					}
 				}
-				if (connected) {break;}
-			}
-			return connected;
+				if (letter.getValue().getY() < 14) {
+					nextTile = board.getTile(letter.getValue().getNear('D'));
+					if (!Game.getCurrentMove().getPlayedTiles().containsKey(nextTile)) {
+						if (nextTile != null) {
+							allowed = true;
+							break;
+						}
+					}
+				}
+				nextTile = board.getTile(letter.getValue().getNear('L'));
+				System.out.println("Tile left is " + (nextTile == null ? "empty" : nextTile.toString()));
+				if (!Game.getCurrentMove().getPlayedTiles().containsKey(nextTile)) {
+					if (nextTile != null) {
+						allowed = true;
+						break;
+					}
+				}
 
+				if (letter.getValue().getX() < 14) {
+					nextTile = board.getTile(letter.getValue().getNear('R'));
+					if (!Game.getCurrentMove().getPlayedTiles().containsKey(nextTile)) {
+						if (nextTile != null) {
+							allowed = true;
+							break;
+						}
+					}
+				}
+
+				continue;
+			}
+			if (allowed) {
+				return true;
+			}
 		}
-		*/
+		return false;
+
+		/*
+		 * if(Game.getMoveList().size() == 1) { return true; } else { for
+		 * (Entry<Tile, Coordinate> entry : word.entrySet()) { Coordinate
+		 * coordinate = entry.getValue(); coordinates.add(coordinate); } for
+		 * (Coordinate coordinate : coordinates) { for (Coordinate neighbour :
+		 * getSurroundingCoordinates(coordinate)) { if
+		 * (!coordinates.contains(neighbour)) { connected = true; break; } } if
+		 * (connected) {break;} } return connected;
+		 * 
+		 * }
+		 */
 	}
-	
+
 	private ArrayList<Coordinate> getSurroundingCoordinates(Coordinate coordinate) {
 		ArrayList<Coordinate> surroundings = new ArrayList<>();
 		if (coordinate.getX() == 0) {
@@ -258,53 +269,57 @@ public class NewValidator implements Serializable {
 			}
 		}
 		return surroundings;
-		
+
 	}
-	
+
 	private Letter getLetter(Coordinate coordinate) {
 		return new Letter(Board.getInstance().getTile(coordinate), coordinate);
 	}
-	
+
 	private Coordinate cLeft(Coordinate coordinate) {
 		return new Coordinate(coordinate.getY() - 1, coordinate.getX());
 	}
-	
+
 	private Coordinate cRight(Coordinate coordinate) {
 		return new Coordinate(coordinate.getY() + 1, coordinate.getX());
 	}
-	
+
 	private Coordinate cAbove(Coordinate coordinate) {
 		return new Coordinate(coordinate.getY(), coordinate.getX() - 1);
 	}
-	
+
 	private Coordinate cBelow(Coordinate coordinate) {
 		return new Coordinate(coordinate.getY(), coordinate.getX() + 1);
 	}
-	
+
 	public void resetDirection() {
 		this.direction = null;
 		this.playedTiles = 1;
 	}
-	
+
 	private void getWords(Letter letter) {
 		this.words = ops.identifyWords(letter);
-	} 
+	}
 
 	public Result getLastResult() {
 		return this.result;
 	}
-	
+
 	private int testMove(Letter letter) {
-		if (words == null) { getWords(letter); }
+		if (words == null) {
+			getWords(letter);
+		}
 		int possibleWords = 0;
 		for (String word : words) {
-			possibleWords += 1;//this.dictionary.getPossibleWords(word);
+			possibleWords += 1;// this.dictionary.getPossibleWords(word);
 		}
 		return possibleWords;
 	}
-	
+
 	private boolean testEndTurnMove(Letter letter) {
-		if (words == null) { getWords(letter); }
+		if (words == null) {
+			getWords(letter);
+		}
 		int completeWords = 0;
 		for (String word : words) {
 			completeWords += dictionary.getCompleteWords(word);
