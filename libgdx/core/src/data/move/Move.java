76e04cdead02232dd172f62b8c09fd3ea385ac85
Ben <Ben@bmiller>
2018-04-29 19:52:15 +0100

    fixing bug cant plan on bottom or far right rows

--- libgdx/core/src/data/move/Move.java
+++ libgdx/core/src/data/move/Move.java
@@ -18,131 +18,134 @@ import java.util.*;
  * @version 1.2
  */
 public abstract class Move implements Serializable {
-    final static Score SCORE_CALCULATOR = new Score();
-    final HashMap<Tile, Coordinate> playedTiles = new HashMap<>();
-    final Board board = Board.getInstance();
-    final Player player;
-    String playedWord = "";
-    int wordMultiplier = 1;
-    int moveTime;
-    int moveScore;
+	final static Score SCORE_CALCULATOR = new Score();
+	final HashMap<Tile, Coordinate> playedTiles = new HashMap<>();
+	final Board board = Board.getInstance();
+	final Player player;
+	String playedWord = "";
+	int wordMultiplier = 1;
+	int moveTime;
+	int moveScore;
 
+	public Move(Player player) {
+		this.player = player;
+	}
 
-    public Move(Player player) {
-        this.player = player;
-    }
+	/* PLAYED TILES */
 
-    /* PLAYED TILES */
+	public abstract void addTile(Tile tile, Coordinate coordinate);
 
-    public abstract void addTile(Tile tile, Coordinate coordinate);
+	public abstract void removeTile(Tile tile);
 
-    public abstract void removeTile(Tile tile);
+	/**
+	 * Returns all Tiles played in this move.
+	 *
+	 * @return tiles Tiles played in this move.
+	 */
+	public HashMap<Tile, Coordinate> getPlayedTiles() {
+		return playedTiles;
+	}
 
-    /**
-     * Returns all Tiles played in this move.
-     *
-     * @return tiles        Tiles played in this move.
-     */
-    public HashMap<Tile, Coordinate> getPlayedTiles() {
-        return playedTiles;
-    }
+	/* PLAYED WORD */
 
+	/**
+	 * Set the word played in this move.
+	 *
+	 * @param playedWord
+	 *            The word played in this move.
+	 */
+	public void setPlayedWord(String playedWord) {
+		this.playedWord = playedWord;
+	}
 
-    /* PLAYED WORD */
+	/**
+	 * Returns the word played in this move
+	 *
+	 * @return word The played word
+	 */
+	public String getPlayedWord() {
+		return playedWord;
+	}
 
-    /**
-     * Set the word played in this move.
-     *
-     * @param playedWord The word played in this move.
-     */
-    public void setPlayedWord(String playedWord) {
-        this.playedWord = playedWord;
-    }
+	/* PLAYER */
 
-    /**
-     * Returns the word played in this move
-     *
-     * @return word               The played word
-     */
-    public String getPlayedWord() {
-        return playedWord;
-    }
+	/**
+	 * Returns the Player of this move.
+	 *
+	 * @return player Player who playing/played this move.
+	 */
+	public Player getPlayer() {
+		return player;
+	}
 
+	/* SCORE */
 
-    /* PLAYER */
+	/**
+	 * Returns current score of move.
+	 *
+	 * @return score Current score of move.
+	 */
+	public int getMoveScore() {
+		return moveScore * wordMultiplier;
+	}
 
-    /**
-     * Returns the Player of this move.
-     *
-     * @return player      Player who playing/played this move.
-     */
-    public Player getPlayer() {
-        return player;
-    }
+	/* MOVE FUNCTIONS */
+	public void endMove() {
+		moveTime = Game.getTimer().getTime();
+		for (Coordinate coordinate : playedTiles.values()) {
+			moveScore += calculateConnectedTileScores(coordinate);
+		}
+		player.setScore(player.getScore() + getMoveScore());
+		player.addTiles();
+	}
 
+	private int calculateConnectedTileScores(Coordinate coordinate) {
+		Coordinate tempCoordinate = coordinate.getNear('U');
+		int tempScore = 0;
 
-    /* SCORE */
+		if (!playedTiles.containsKey(board.getTile(tempCoordinate)) && tempCoordinate.getY() >= 0
+				&& board.getTile(tempCoordinate) != null) {
+			tempScore += board.getTile(tempCoordinate).getScore();
+		}
 
-    /**
-     * Returns current score of move.
-     *
-     * @return score       Current score of move.
-     */
-    public int getMoveScore() {
-        return moveScore * wordMultiplier;
-    }
+		tempCoordinate = coordinate.getNear('D');
 
-    /* MOVE FUNCTIONS */
-    public void endMove() {
-        moveTime = Game.getTimer().getTime();
-        for (Coordinate coordinate : playedTiles.values()) {
-            moveScore += calculateConnectedTileScores(coordinate);
-        }
-        player.setScore(player.getScore() + getMoveScore());
-        player.addTiles();
-    }
+		if (tempCoordinate.getY() <= 14) {
+			if (!playedTiles.containsKey(board.getTile(tempCoordinate)) && board.getTile(tempCoordinate) != null) {
+				tempScore += board.getTile(tempCoordinate).getScore();
+			}
+		}
+		tempCoordinate = coordinate.getNear('L');
 
-    private int calculateConnectedTileScores(Coordinate coordinate) {
-        Coordinate tempCoordinate = coordinate.getNear('U');
-        int tempScore = 0;
+		if (!playedTiles.containsKey(board.getTile(tempCoordinate)) && tempCoordinate.getX() >= 0
+				&& board.getTile(tempCoordinate) != null) {
+			tempScore += board.getTile(tempCoordinate).getScore();
+		}
 
-        if (!playedTiles.containsKey(board.getTile(tempCoordinate)) && tempCoordinate.getY() >= 0 && board.getTile(tempCoordinate) != null) {
-            tempScore += board.getTile(tempCoordinate).getScore();
-        }
+		tempCoordinate = coordinate.getNear('R');
 
-        tempCoordinate = coordinate.getNear('D');
+		if (tempCoordinate.getX() <= 14) {
+			if (!playedTiles.containsKey(board.getTile(tempCoordinate)) && board.getTile(tempCoordinate) != null) {
+				tempScore += board.getTile(tempCoordinate).getScore();
 
-        if (!playedTiles.containsKey(board.getTile(tempCoordinate)) && tempCoordinate.getY() <= 14 && board.getTile(tempCoordinate) != null) {
-            tempScore += board.getTile(tempCoordinate).getScore();
-        }
+			}
+		}
 
-        tempCoordinate = coordinate.getNear('L');
+		return tempScore;
+	}
 
-        if (!playedTiles.containsKey(board.getTile(tempCoordinate)) && tempCoordinate.getX() >= 0 && board.getTile(tempCoordinate) != null) {
-            tempScore += board.getTile(tempCoordinate).getScore();
-        }
+	public void invalidateMove() {
+		moveScore = 0;
+		playedWord = "";
+		moveTime = Game.getTimer().getTime();
 
-        tempCoordinate = coordinate.getNear('R');
+		HashMap<Tile, Coordinate> tempMap = new HashMap<>(playedTiles);
+		for (Tile tile : tempMap.keySet()) {
+			removeTile(tile);
+		}
+	}
 
-        if (!playedTiles.containsKey(board.getTile(tempCoordinate)) && tempCoordinate.getX() <= 14 && board.getTile(tempCoordinate) != null) {
-            tempScore += board.getTile(tempCoordinate).getScore();
-        }
-
-        return tempScore;
-    }
-
-    public void invalidateMove() {
-        moveScore = 0;
-        playedWord = "";
-        moveTime = Game.getTimer().getTime();
-
-        HashMap<Tile, Coordinate> tempMap = new HashMap<>(playedTiles);
-        for (Tile tile : tempMap.keySet()) {
-            removeTile(tile);
-        }
-    }
-
-    public int getMoveTime() {
-        return moveTime;
-    }
+	public int getMoveTime() {
+		return moveTime;
+	}
 }
